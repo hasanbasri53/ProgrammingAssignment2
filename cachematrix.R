3f77a6bf1a4aeb937e8b001187628389ac99a425
hasanbasri53 <hasanbasri53@gmail.com>
2020-12-26 12:26:06 +0300

    The solution was added

--- cachematrix.R
+++ cachematrix.R
@@ -8,7 +8,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
